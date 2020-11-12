35a6a5ffbde479f7d58b74599916628bbcd8fc83
gregbm <32569979+gregbm@users.noreply.github.com>
2020-11-12 09:35:28 +0100

    Create welibs.go

---
+++ welibs.go
@@ -0,0 +1,78 @@
+package welibs
+
+import ("fmt"
+    "crypto/aes"
+    "crypto/cipher"
+            
+    "crypto/rand"
+           
+    "encoding/base64"
+    "io"
+  
+    "runtime"
+    "os"
+    "os/exec"
+)
+// This func must be Exported, Capitalized, and comment added.
+func Demo(ausgabe string) {
+    fmt.Println(ausgabe)
+}
+
+func Encrypt(key []byte, text string) string {
+	// key := []byte(keyText)
+	plaintext := []byte(text)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
+}
+func Decrypt(key []byte, cryptoText string) string {
+	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// XORKeyStream can work in-place if the two arguments are the same.
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return fmt.Sprintf("%s", ciphertext)
+}
+func Writetoenv(keyname string, keyvalue string){
+    if runtime.GOOS == "windows" {
+         exec.Command("powershell","setx /m",keyname+" "+keyvalue).Output()
+    }   
+}
+
+func Readfromenv(key []byte, keyname string) string{
+ 
+    KeyID := os.Getenv(keyname)
+    return KeyID
+    
+}
