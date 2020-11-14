66093d57c3a24c48fc00673b8a072a0108793c23
gregbm <32569979+gregbm@users.noreply.github.com>
2020-11-14 16:19:39 +0100

    Update welibs.go

--- welibs.go
+++ welibs.go
@@ -79,11 +79,11 @@ func Readfromenv(key []byte, keyname string) string{
     return KeyID
     
 }
-func GetSystem(){
+func GetSystem() string{
 	dll := syscall.MustLoadDLL("kernel32.dll")
 	p := dll.MustFindProc("GetVersion")
 	v, _, _ := p.Call()
-	fmt.Printf("Windows version %d.%d (Build %d)\n", byte(v), uint8(v>>8), uint16(v>>16))
+	return (fmt.Printf("Windows version %d.%d (Build %d)\n", byte(v), uint8(v>>8), uint16(v>>16)))
 }
 func ValidateId(){
 	id, err := machineid.ID()
